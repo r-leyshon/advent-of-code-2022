eb4bf11522f568170a0c237b608c6b3705de4373
r-leyshon <richard.leyshon@ons.gov.uk>
2022-12-03 06:56:56 +0000

    refactor: Use  a priority module score

--- 3.rucksacks/challenge.py
+++ 3.rucksacks/challenge.py
@@ -9,6 +9,28 @@ with open(os.path.join(here(), "data", "3.rucksacks.txt"), "r") as f:
 
 sacks = txt.splitlines()
 
+
+def elfPriority(letter):
+    """Return the elf priority score for any upper or lowercase letter.
+
+    Args:
+        letter (str): A-Za-z
+
+    Returns:
+        int: The priority score of the item type.
+    """
+    # get ascii value and convert to elf score
+    dupeVal = ord(letter)
+    # handle case letters
+    if dupeVal < 91:
+        # UPPERCASE
+        elfScore = dupeVal - 38
+    elif dupeVal > 90:
+        # lowercase
+        elfScore = dupeVal - 96
+    return elfScore
+
+
 dupeVals = []
 for sack in sacks:
     # split the sack into 2 compartments
@@ -30,15 +52,7 @@ for sack in sacks:
     print(f"All dupes: {dupes}")
     # get ascii value and convert to elf score
     for dup in dupes:
-        dupeVal = ord(dup)
-        print(f"ascii: {dupeVal}")
-        # handle case letters
-        if dupeVal < 91:
-            # UPPERCASE
-            elfScore = dupeVal - 38
-        elif dupeVal > 90:
-            # lowercase
-            elfScore = dupeVal - 96
+        elfScore = elfPriority(dup)
         dupeVals.append(elfScore)
 
 print(f"the sum of all the misplaced item scores is {sum(dupeVals):,}.")
