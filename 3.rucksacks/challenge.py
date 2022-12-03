f2bc4fada84cf1c239fb8454a4edb8b18e0d051b
r-leyshon <richard.leyshon@ons.gov.uk>
2022-12-03 08:07:16 +0000

    feat: Day 3 part 2

--- 3.rucksacks/challenge.py
+++ 3.rucksacks/challenge.py
@@ -9,6 +9,7 @@ with open(os.path.join(here(), "data", "3.rucksacks.txt"), "r") as f:
 
 sacks = txt.splitlines()
 
+# part 1
 
 def elfPriority(letter):
     """Return the elf priority score for any upper or lowercase letter.
@@ -38,22 +39,44 @@ for sack in sacks:
     divider = int(n/2)
     c1 = sack[0:divider]
     c2 = sack[divider:n]
-    print(f"c1: {c1}")
-    print(f"c2: {c2}")
     # check for item occurences between compartments
     c1unique = list(set(c1))
     c2unique = list(set(c2))
     dupes = []
     for item in c1unique:
         if item in c2unique:
-            print(f"{item} is a dupe!!")
             dupe = item
             dupes.append(dupe)
-    print(f"All dupes: {dupes}")
     # get ascii value and convert to elf score
     for dup in dupes:
         elfScore = elfPriority(dup)
         dupeVals.append(elfScore)
 
 print(f"the sum of all the misplaced item scores is {sum(dupeVals):,}.")
+
+# part 2
+# get all the elf trios
         
+count = 0
+badgeVals = []
+for entry in range(0, len(sacks) + 1):
+    if (count % 3) != 0:
+        pass
+    elif count == 0:
+        pass
+    else:
+        startInd = count - 3
+        team = sacks[startInd:count]
+        # get the unique items in each rucksack
+        teamUnique = []
+        for sack in team:
+            teamUnique.append("".join(set(sack)))
+        # find the badges
+        for item in teamUnique[0]:
+            if item in teamUnique[1] and item in teamUnique[2]:
+                badge = item
+                badgeVal = elfPriority(badge)
+                badgeVals.append(badgeVal)
+    count += 1
+
+print(f"the sum of all the elves' badge values is {sum(badgeVals):,}")
