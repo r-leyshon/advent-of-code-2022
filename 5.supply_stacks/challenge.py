7a40cf5af5d707948a21adb3fb20a94b5b476acd
r-leyshon <richard.leyshon@ons.gov.uk>
2022-12-05 07:13:24 +0000

    feat: Sorting out the crates

---
+++ 5.supply_stacks/challenge.py
@@ -0,0 +1,41 @@
+import os
+from pyprojroot import here
+
+# https://adventofcode.com/2022/day/5
+# Part 1
+# After the rearrangement procedure completes, what crate ends up on top of each stack?
+
+# import
+with open(os.path.join(here(), "data", "5.crates.txt")) as f:
+    txt = f.read()
+    f.close()
+
+crates, guide = txt.split("\n\n")
+# too many spaces to see gaps in crates
+crates = crates.replace(" ", "-")
+crates = crates.splitlines()
+# separate out the boxes
+allRows = list()
+for row in crates:
+    start = 0
+    stop = 3
+    currRow = list()
+    print(row[start:stop])
+    for i in range(0, 9):
+        contents = "".join(row[start:stop]).strip()
+        print(contents)
+        currRow.append(contents)
+        start += 4
+        stop += 4
+    print(currRow)
+    allRows.append(currRow)
+
+stackLabels = allRows.pop(-1)
+# stack the crates as list sequences
+stacks = list()
+for ind, stack in enumerate(stackLabels):
+    column = list()
+    for row in allRows:
+        column.append(row[ind])
+    column.append(stack)
+    stacks.append(column)
