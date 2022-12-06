d3c9668e4e97b73fab667de9245f3d96c2ffdb1c
r-leyshon <richard.leyshon@ons.gov.uk>
2022-12-06 21:03:22 +0000

    feat: Solution to day 6 pt 2

--- 6.tuning-trouble/challenge.py
+++ 6.tuning-trouble/challenge.py
@@ -4,19 +4,27 @@ import os
 with open(os.path.join(here(), "data", "6.signal.txt"), "r") as f:
     txt = f.read()
     f.close()
-
 # part 1
 # How many characters need to be processed before the first start-of-packet marker is
 # detected?
-for ind, char in enumerate(txt):
-    # search for unique combos of 4 digits
-    if ind > 2:
-        pat = txt[ind-3:ind+1]
-        if len(set(pat)) == 4:
-            print(f"Start of packet marker is {pat}")
-            print(f"{ind + 1} characters were processed.")
-            break
+
+
+def get_n_processed(txt=txt, seqLen=4):
+    """Return n characters processed in sequence to reach a unique set of seqLen length.
+
+    Args:
+        txt (str, optional): Sequence of characters. Defaults to txt.
+        seqLen (int, optional): The unique sequence length. Defaults to 4.
+    """
+    for ind, char in enumerate(txt):
+        pat = txt[ind - (seqLen - 1):ind + 1]
+        if len(set(pat)) == seqLen:
+            return(ind + 1)
         else:
             continue
-            
-        
+
+
+print(f"Characters processed: {get_n_processed()}")
+# part 2
+# Change seqLen to 14
+print(f"Characters processed: {get_n_processed(seqLen=14)}")
