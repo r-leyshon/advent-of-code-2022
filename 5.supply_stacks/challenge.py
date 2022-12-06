248331619d4484320d3be896be6fa30c3d0b55c4
r-leyshon <richard.leyshon@ons.gov.uk>
2022-12-06 18:29:41 +0000

    feat: Day 5 part 2

--- 5.supply_stacks/challenge.py
+++ 5.supply_stacks/challenge.py
@@ -1,6 +1,7 @@
 import os
 from pyprojroot import here
 import re
+import copy
 
 # https://adventofcode.com/2022/day/5
 # Part 1
@@ -53,6 +54,8 @@ def get_top_box(stack):
         tuple: (int) The index of the topmost crate, (str) The topmost
         crate content.
     """
+    if len(stack) == 0:
+        raise ValueError(f"stack is empty.")
     for ind, crate in enumerate(stack):
         if crate == "---":
             pass
@@ -92,6 +95,7 @@ def move_crates(stacks, rule, crateModel="CrateMover 9000"):
     Returns:
         None: Stacks will be updated in place.
     """
+    print(f"Processing rule: {rule}")
     n, start, dest = interpret_rule(rule)   
     ind, crate = get_top_box(stacks[start])
     movingCrates = stacks[start][ind:ind + n]
@@ -106,11 +110,13 @@ def move_crates(stacks, rule, crateModel="CrateMover 9000"):
     return None
 
 
-for rule in guide:
-    move_crates(stacks, rule)
+# make a copy of stacks to preserve setup for solution 2
+stacks_1 = copy.deepcopy(stacks)
+for rule in guide[0:1]:
+    move_crates(stacks_1, rule)
 
 solution = []
-for stack in stacks:
+for stack in stacks_1:
     n, topBox = get_top_box(stack)
     solution.append(topBox)
 
@@ -119,4 +125,13 @@ print(
     )
 
 # Part 2
+stacks_2 = copy.deepcopy(stacks)
+for rule in guide:
+    move_crates(stacks_2, rule, crateModel="CrateMover 9001")
 
+solution2 = []
+for stack in stacks_2:
+    n, topBox = get_top_box(stack)
+    solution2.append(topBox.replace("[", "").replace("]", ""))
+
+print(f"The top crates are {''.join(solution2)}")
