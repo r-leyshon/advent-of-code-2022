ac18a835850780c673764f395dee4dabb7466ec1
r-leyshon <richard.leyshon@ons.gov.uk>
2022-12-10 07:38:49 +0000

    feat: Day 7 part 1 solution

---
+++ 7.space_on_device/challenge.py
@@ -0,0 +1,71 @@
+import os
+from pyprojroot import here
+import tempfile
+
+with open(os.path.join(here(), "data", "7.fileOps.txt"), "r") as f:
+    txt = f.read()
+    f.close()
+fileOps = txt.splitlines()
+# Part 1
+# Find all of the directories with a total size of at most 100000. What is the sum of
+# the total sizes of those directories?
+dirKey = str()
+curwd = here()
+tmp = tempfile.TemporaryDirectory(dir=curwd)
+
+for ind, op in enumerate(fileOps):
+    print(f"Line {ind}: {op}")
+    if op.startswith("$"):
+        if "$ cd /" in op:
+            os.chdir(tmp.name)
+            targetDir = tmp.name
+        elif "$ cd " in op:
+            targetDir = op.split(" ")[-1]
+            dirKey = targetDir
+            os.chdir(targetDir)
+    elif op.startswith("dir"):
+        target = os.path.join(os.getcwd(), op.split(" ")[-1])
+        print(f"Creating directory {target}")
+        os.mkdir(target)
+    else:
+        size, filenm = op.split(" ")
+        target = os.path.join(os.getcwd(), filenm)
+        print(f"Write file to {target}")
+        f = open(target, "w")
+        f.write(size)
+        f.close()
+
+# go back to pyprojroot
+os.chdir(curwd)
+
+
+def sum_file_size(directory):
+    """Recursively reads files in a directory and sums the integers in
+    contents.
+
+    Args:
+        directory (path): Path to the directory.
+
+    Returns:
+        int: Sum of the found integers.
+    """
+    sizes = list()
+    for root, dirs, files in os.walk(directory):
+        for name in files:
+            with open(os.path.join(root, name), "r") as f:
+                size = int(f.read())
+                sizes.append(size)
+                f.close()
+    return sum(sizes)
+
+
+smaller_100k = list()
+# get all the available directories
+for root, dirs, files in os.walk(tmp.name, topdown=False):
+    for dir in dirs:
+        tot = sum_file_size(os.path.join(root, dir))
+        if tot <= 100000:
+            smaller_100k.append(tot)
+
+print(f"The sum of the folders with size at most 100k: {sum(smaller_100k)}")
+tmp.cleanup()
