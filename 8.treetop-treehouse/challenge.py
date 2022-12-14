a2c1fb540cd820b4de17d1264175c1926cdf87dd
r-leyshon <richard.leyshon@ons.gov.uk>
2022-12-14 19:54:13 +0000

    feat: Day 8 part 2

--- 8.treetop-treehouse/challenge.py
+++ 8.treetop-treehouse/challenge.py
@@ -47,6 +47,7 @@ def is_visible(tree, ind, stand):
     
     return isViz
         
+
 allRows = list()
 for stand in trees:
     rowViz = []
@@ -66,3 +67,65 @@ y = np.asarray(allCols).T
 
 print(f"There are {sum(sum(x + y))} visible trees.")
 
+# Part 2: Consider each tree on your map. What is the highest scenic score
+# possible for any tree?
+
+
+def scenic_score(tree, ind, stand):
+    """Calculate the scenic score of a tree.
+
+    Args:
+        tree (int): Height value of the tree.
+        ind (int): Index of the tree in the stand.
+        stand (numpy.ndarray): Array containing height integers of trees.
+
+    Returns:
+        int: Product of the score from both views in a stand.
+    """
+    comps = [tree <= x for i, x in enumerate(stand)]
+    # subset comparisons but omit the tree that is the current ind
+    side1 = comps[0:ind]
+    side2 = comps[ind+1:len(stand)]
+
+    if ind == 0:
+        leftScore = 0
+    elif all(item == False for item in side1):
+        leftScore = len(side1)
+    else:
+        viewL = [i for i, x in enumerate(np.flip(side1)) if x]
+        # Add 1 because the blocking tree is also visible
+        leftScore = viewL[0] + 1
+    
+    if len(side2) == 0:
+        rightScore = 0
+    elif all(item == False for item in side2):
+        rightScore = len(side2)
+    else:
+        # if index is neg, return 0
+        rightScore = max(side2.index(True) + 1, 0)
+    
+    return leftScore * rightScore
+
+
+rowScores = []
+for stand in trees:
+    rowScore = []
+    for ind, tree in enumerate(stand):
+        rowScore.append(scenic_score(tree, ind, stand))
+    rowScores.append(rowScore)
+
+colScores = []
+for stand in trees.T:
+    colScore = []
+    for ind, tree in enumerate(stand):
+        colScore.append(scenic_score(tree, ind, stand))
+    colScores.append(colScore)
+
+x = np.asarray(rowScores)
+y = np.asarray(colScores).T
+z = x * y
+maxes = list()
+for r in z:
+    maxes.append(max(r))
+
+print(f"The highest scenic score is {max(maxes)}.s")
